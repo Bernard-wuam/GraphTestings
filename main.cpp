101eb6286ee873867c56d9b5f1cb22878b078a78
Bernard-wuam <wuambernard@yahoo.com>
2023-03-15 14:00:44 +0100

    first commit

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <QCoreApplication>
+#include "adjacencylistgraph.h"
+#include "adjacencylistgraph.cpp"
+
+int main(int argc, char *argv[])
+{
+  QCoreApplication a(argc, argv);
+  ADjacencyListGraph ad
+  {
+    {'A',{{'B',34},{'C',4},{'D',24}}},
+    {'C',{{'B',34}}},
+    {'F',{{'B',34}}}
+  };
+  ad.breathFirstTransversal ();
+  return a.exec();
+}
